54aa92b67e31b8f4fdc2e0c6be7a5039d58f9195
rezo552 <43187470+rezo552@users.noreply.github.com>
2023-11-05 10:38:37 +0000

    I cannot even do basic stuff

--- supportFiles/build.sh
+++ supportFiles/build.sh
@@ -33,9 +33,9 @@ chown -v root:root $HOME/LIVE_BOOT/chroot/etc/systemd/network/99-dhcp-en.network
 chmod -v 644 $HOME/LIVE_BOOT/chroot/etc/systemd/network/99-dhcp-en.network
 
 echo Copy in systemd config
-cp -v /supportFiles/sysbackup.service $HOME/LIVE_BOOT/chroot/etc/systemd/sysbackup.service
-chown -v root:root $HOME/LIVE_BOOT/chroot/etc/systemd/sysbackup.service
-chmod -v 644 $HOME/LIVE_BOOT/chroot/etc/systemd/sysbackup.service
+cp -v /supportFiles/sysbackup.service $HOME/LIVE_BOOT/chroot/etc/systemd/system/sysbackup.service
+chown -v root:root $HOME/LIVE_BOOT/chroot/etc/systemd/system/sysbackup.service
+chmod -v 644 $HOME/LIVE_BOOT/chroot/etc/systemd/system/sysbackup.service
 
 echo Enable autologin
 mkdir -p -v $HOME/LIVE_BOOT/chroot/etc/systemd/system/getty@tty1.service.d/
